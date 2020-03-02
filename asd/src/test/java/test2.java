9492ed25459a5ea3bfe13e215839d89f79462dda
Curtis <814909775@qq.com>
2020-03-02 21:08:02 +0800

    第二次

--- asd/src/test/java/test2.java
+++
@@ -1,9 +0,0 @@
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-import org.testng.Assert;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-
-public class test2 {
-
-}
--- asd/target/test-classes/resources/Test2.feature
+++
@@ -1,8 +0,0 @@
-Feature: this is test2
-  @2
-  Scenario Outline: test2
-    When inputaa "<a>","<b>","<c>"
-    Then aab
-    Examples:
-      | a | b | c |
-      |1  |2  | 3  |
